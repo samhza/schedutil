ee2ed0262376cda5394074be0ef015d078c235f8
Samuel Hernandez <sam@samhza.com>
2024-02-20 22:10:41 -0500

    add flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "nixpkgs";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in rec {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          (python3.withPackages(ps: with ps; [
+            python-lsp-server
+            aiohttp
+          ]))
+        ];
+      };
+    }
+  );
+}
